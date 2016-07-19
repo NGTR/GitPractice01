1010428db53b475935aed91e6f9bdc18efac8d02
negitoro <fatigue20@gmail.com>
2016-07-19 08:27:40 -0700

    Driver.h added

---
+++ Driver.h
@@ -0,0 +1,23 @@
+#ifndef SICT_DRIVER_H_
+#define SICT_DRIVER_H_
+#include "Car.h"
+
+namespace gitp {
+	extern std::ostream&operator<<(std::ostream& oss, const sict::Car& carName);
+
+	class Driver : public Car {
+	private:
+		char name_[31];
+		Car& car_;
+	public:
+		Driver(const char* name, Car& cRef);
+
+		void drive();
+		void stop();
+		std::ostream& display(std::ostream& os) const;
+		void showStatus();
+		void accelerate() {}
+		void brake() {}
+	};
+}
+#endif
